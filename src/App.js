4f4817ffdf9b6871e846caeb2165e00b2026829c
margo.bezmosciuc <margo.bezmosciuc@gmail.com>
2023-04-10 16:46:46 +0200

    Exercise Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Users from './components/Users';
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'MarGo' },
+  { id: 'u2', name: 'Kataleya' },
+  { id: 'u3', name: 'Dusika' },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS
+  }
+
   return (
-    <div>
-      <Users />
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
 
